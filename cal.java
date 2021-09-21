fc0d13180e3a43a12a46e84e835dbd5e1b1ab646
hemantraja19 <hemantraja19@gmail.com>
2021-09-21 17:45:55 +0530

    first

--- cal.java
+++ cal.java
@@ -2,9 +2,9 @@ public class cal{
 	public static void main(String[] args){
 		int a = 10;
 		int b = 20;
-		int c;
-		c = a+b;
-		System.out.print(c);
+		int d;
+		d = a+b;
+		System.out.print(d);
 	}
 }
 
